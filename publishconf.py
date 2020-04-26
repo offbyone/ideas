f55a32e453efa9723eaab39674cd3cc27657d800
Chris Rose <offline@offby1.net>
2020-04-26 10:06:29 -0700

    Upgrade Pelican to 4.2 - publication settings

--- publishconf.py
+++ publishconf.py
@@ -7,15 +7,16 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = '//ideas.offby1.net'
+SITEURL = "//ideas.offby1.net"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
-TAG_FEED_ATOM = 'feeds/tag-%s.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+TAG_FEED_ATOM = "feeds/tag-{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
