b169f642019149274091fc5b70969e1912de8223
Chris Rose <offline@offby1.net>
2020-05-15 21:59:39 -0700

    Fix my site feeds to hold only 15 items

--- publishconf.py
+++ publishconf.py
@@ -14,6 +14,8 @@ from pelicanconf import *
 SITEURL = "//ideas.offby1.net"
 RELATIVE_URLS = False
 
+FEED_DOMAIN = "https:" + SITEURL
+FEED_MAX_ITEMS = 15
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 TAG_FEED_ATOM = "feeds/tag-{slug}.atom.xml"
