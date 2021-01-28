9a895320a58b95981c98ed8c298c30af92e79591
Chris Rose <offline@offby1.net>
2021-01-27 22:24:09 -0800

    Remove obsolete travis script

--- update-travis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-travis encrypt AWS_SECRET_ACCESS_KEY=$(terraform output blog_iam_user_aws_secret_key) --override --add env.global
-travis encrypt AWS_ACCESS_KEY_ID=$(terraform output blog_iam_user_aws_access_key) --append --add env.global
