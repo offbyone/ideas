e8548a484aeb0abe9c9b0f6bb5e72a74f63a0ba6
Chris Rose <offline@offby1.net>
2021-03-12 23:48:16 -0800

    A working tag cloud

---
+++ themes/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil (eval (lambda () (when (string= (file-name-extension buffer-file-name) "html") (jinja2-mode))))))
