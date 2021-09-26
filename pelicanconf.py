5c7dead533e49ae9ce272838e2c5d014b4cefdc1
Chris Rose <offline@offby1.net>
2021-09-26 11:13:15 -0700

    Remove the pelican-plugins meta-repo
    I don't use anything from it any more. No sense it cloning it!

--- pelicanconf.py
+++ pelicanconf.py
@@ -57,7 +57,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-PLUGIN_PATHS = ["./plugins", "./pelican-plugins"]
+PLUGIN_PATHS = ["./plugins"]
 PLUGINS = [
     "webassets",
     "pelican_gist",
